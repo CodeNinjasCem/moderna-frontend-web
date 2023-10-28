1e9b9a3214fdf5219ca42e5a49baf8185b73962e
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-10-28 07:17:25 -0600

    Dockerized

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <>
-      <div className="container relative hidden h-[850px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container h-[850px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 mt-auto">
